21f04b7bb1db0be8a5cab0db90ff04a7c7663c5c
ninja-janin <lukemagsoling@gmail.com>
2018-03-01 21:14:56 +0800

    added header layout

--- gulpfile.js
+++ gulpfile.js
@@ -10,20 +10,20 @@ var gulp        = require('gulp'),
 	reload      = require('browser-sync').reload,
 	util        = require('gulp-util')
 
-gulp.task('default', [ 'sass', 'inject', 'watch', 'go'])
+gulp.task('default', [ 'sass', 'inject', 'watch', 'server'])
 
 gulp.task('reload', reload)
 
 gulp.task('sass', function () {
-  return gulp.src('./src/assets/styles/main.scss')
+  return gulp.src('./assets/sass/main.scss')
     .pipe(glob())
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./assets/css'))
 })
 
 gulp.task('inject', function () {
-	var target  = gulp.src('./src/app/main/index.html')
-	var sources = gulp.src(['./src/**/*.js', './css/**/*.css'], {read: false})
+	var target  = gulp.src('./assets/html/index.html')
+	var sources = gulp.src(['./assets/**/*.js', './assets/css/**/*.css'], {read: false})
 	var bower   = gulp.src(mbf(), { read: false })
 
 	return target
@@ -36,12 +36,12 @@ gulp.task('reload:inject', [ 'inject' ], reload)
 gulp.task('reload:sass', [ 'sass', 'inject'], reload)
 
 gulp.task('watch', function () {
-	gulp.watch('src/app/**/*.js', ['reload:inject'])
-	gulp.watch('src/app/**/*.html', ['reload:inject'])
-	gulp.watch('src/assets/**/*.scss', ['reload:sass'])
+	gulp.watch('assets/**/*.js', ['reload:inject'])
+	gulp.watch('assets/**/*.html', ['reload:inject'])
+	gulp.watch('assets/**/*.scss', ['reload:sass'])
 })
 
-gulp.task('go',function(){
+gulp.task('server',function(){
 	browserSync({
 		server:{
 			baseDir: './'
