7adf8d33cc44b41b163f167e406d50e555de7c50
ninja-janin <lukemagsoling@gmail.com>
2018-03-03 17:55:15 +0800

    Added slider functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,70 +1,81 @@
 $(document).ready(() => {
+	let slider_element = $('#photo-slider'),
+		active_index   = 1,
+		image_width    = 33.333,
+		default_slide  = '#pager-' +active_index,
+		self           = this,
+		multiplier
 
-	let gallery    = $('#gallery')
-	let curr_index = 1
-	let pic_width  = 33.333; 
-
-    $('#pager-' + curr_index).attr('src', 'assets/images/hover-indicator.png');
-    $('#previous').hide();
+    $(default_slide).attr('src', 'assets/images/hover-indicator.png')
+    $('#previous').hide()
       
     $('.slider__btn-trigger').on('click', function() {
-        let btn = $(this);
-        let btn_id = btn.attr('id');
-        if (gallery.is(':not(:animated)')) {
-            if(btn_id.split('-')[0] == 'previous' && curr_index == 1) {
-                return false;
-            }
-            if(btn_id.split('-')[0] == 'next' && curr_index == 3) {
-                return false;
-            }
-            if(btn_id.split('-')[1] == curr_index) {
-                return false;
-            }
-
-            let multiplier;       
-
-			if (btn_id == 'previous') {
-				multiplier = -3;
-				curr_index -= 1;
-			}
-			else if(btn_id == 'next'){
-				multiplier = 3;
-				curr_index += 1;
-			}
-			else if(btn_id == 'pager-2'){
-				multiplier = (curr_index == 3) ? -3 : 3;
-				curr_index = 2;
-			}
-			else if(btn_id == 'pager-3'){
-				multiplier = (curr_index == 1) ? 6 : 3;
-				curr_index = 3;
-			}
-			else{
-				multiplier = (curr_index == 2) ? -3 : -6;
-				curr_index = 1;
-			}
-
-            gallery.animate({ left: '+=' + (-pic_width * multiplier + '%') },() => {
-                $('#pager-' + curr_index).attr('src', 'assets/images/hover-indicator.png');
-                $('.slider__btn-indicator').not('#pager-' + curr_index).attr('src', 'assets/images/slide-indicator.png');
-                if(curr_index == 1) {
-                    $('#previous').hide();
-                    $('#next').show();
-                }
-                else if(curr_index == 3) {
-                    $('#previous').show();
-                    $('#next').hide();
-                }
-                else {
-                    $('#previous').show();
-                    $('#next').show();
-                }
-            });   
-        }
-    });
+        let slider_btn_event = $(this)
+        let id = slider_btn_event.attr('id')
+		self.image_preview(slider_btn_event, id)
+    })
 
     $('#btn-pagetop').on('click', () => {
-        $('html, body').animate({ scrollTop: 0 }, 'slow');
-        return false;
-    });
-});
+        $('html, body').animate({ scrollTop: 0 }, 'slow')
+        return false
+    })
+
+	self.image_preview = (slider_btn_event, id) => {
+        if (slider_element.is(':not(:animated)')) {
+            if(id.split('-')[0] == 'previous' && active_index == 1) {
+                return false
+            }
+            if(id.split('-')[0] == 'next' && active_index == 3) {
+                return false
+            }
+            if(id.split('-')[1] == active_index) {
+                return false
+            }
+
+			self.slider_transition(id, active_index)
+			self.slider_animation(active_index)
+        }
+	}
+
+	self.slider_transition = (id) => {
+		if (id == 'previous') {
+			multiplier = -3
+			active_index -= 1
+		}
+		else if(id == 'next'){
+			multiplier = 3
+			active_index += 1
+		}
+		else if(id == 'pager-2'){
+			multiplier = (active_index == 3) ? -3 : 3
+			active_index = 2
+		}
+		else if(id == 'pager-3'){
+			multiplier = (active_index == 1) ? 6 : 3
+			active_index = 3
+		}
+		else{
+			multiplier = (active_index == 2) ? -3 : -6
+			active_index = 1
+		}
+	}
+
+	self.slider_animation = () =>{
+		slider_element.animate({ left: '+=' + (-image_width * multiplier + '%') },() => {
+			$('#pager-' + active_index).attr('src', 'assets/images/hover-indicator.png')
+			$('.slider__btn-indicator').not('#pager-' + active_index).attr('src', 'assets/images/slide-indicator.png')
+			if(active_index == 1) {
+				$('#previous').hide()
+				$('#next').show()
+			}
+			else if(active_index == 3) {
+				$('#previous').show()
+				$('#next').hide()
+			}
+			else {
+				$('#previous').show()
+				$('#next').show()
+			}
+		})   
+	}
+})
