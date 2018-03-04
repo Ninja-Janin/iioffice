e9455687efdf8c8511dcb2e2813cc6c7865ba30c
ninja-janin <lukemagsoling@gmail.com>
2018-03-04 14:49:02 +0800

    fix hover function

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,78 +4,78 @@ $(document).ready(() => {
 		image_width    = 33.333,
 		default_slide  = '#pager-' +active_index,
 		self           = this,
-		multiplier
+		multiplier;
 
-    $(default_slide).attr('src', 'assets/images/hover-indicator.png')
-    $('#previous').hide()
+    $(default_slide).attr('src', 'assets/images/hover-indicator.png');
+    $('#previous').hide();
       
     $('.slider__btn-trigger').on('click', function() {
-        let slider_btn_event = $(this)
-        let id = slider_btn_event.attr('id')
-		self.image_preview(slider_btn_event, id)
-    })
+        let slider_btn_event = $(this);
+        let id = slider_btn_event.attr('id');
+		self.image_preview(slider_btn_event, id);
+    });
 
     $('#scroll-top-btn').on('click', () => {
-        $('html, body').animate({ scrollTop: 0 }, 'slow')
-        return false
-    })
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        return false;
+    });
 
 	self.image_preview = (slider_btn_event, id) => {
         if (slider_element.is(':not(:animated)')) {
             if(id.split('-')[0] == 'previous' && active_index == 1) {
-                return false
+                return false;
             }
             if(id.split('-')[0] == 'next' && active_index == 3) {
-                return false
+                return false;
             }
             if(id.split('-')[1] == active_index) {
-                return false
+                return false;
             }
 
-			self.slider_transition(id, active_index)
-			self.slider_animation(active_index)
+			self.slider_transition(id, active_index);
+			self.slider_animation(active_index);
         }
 	}
 
 	self.slider_transition = (id) => {
 		if (id == 'previous') {
-			multiplier = -3
-			active_index -= 1
+			multiplier = -3;
+			active_index -= 1;
 		}
 		else if(id == 'next'){
-			multiplier = 3
-			active_index += 1
+			multiplier = 3;
+			active_index += 1;
 		}
 		else if(id == 'pager-2'){
-			multiplier = (active_index == 3) ? -3 : 3
-			active_index = 2
+			multiplier = (active_index == 3) ? -3 : 3;
+			active_index = 2;
 		}
 		else if(id == 'pager-3'){
-			multiplier = (active_index == 1) ? 6 : 3
-			active_index = 3
+			multiplier = (active_index == 1) ? 6 : 3;
+			active_index = 3;
 		}
 		else{
-			multiplier = (active_index == 2) ? -3 : -6
-			active_index = 1
+			multiplier = (active_index == 2) ? -3 : -6;
+			active_index = 1;
 		}
 	}
 
 	self.slider_animation = () =>{
 		slider_element.animate({ left: '+=' + (-image_width * multiplier + '%') },() => {
-			$('#pager-' + active_index).attr('src', 'assets/images/hover-indicator.png')
-			$('.slider__btn-indicator').not('#pager-' + active_index).attr('src', 'assets/images/slide-indicator.png')
+			$('#pager-' + active_index).attr('src', 'assets/images/hover-indicator.png');
+			$('.slider__btn-indicator').not('#pager-' + active_index).attr('src', 'assets/images/slide-indicator.png');
 			if(active_index == 1) {
-				$('#previous').hide()
-				$('#next').show()
+				$('#previous').hide();
+				$('#next').show();
 			}
 			else if(active_index == 3) {
-				$('#previous').show()
-				$('#next').hide()
+				$('#previous').show();
+				$('#next').hide();
 			}
 			else {
-				$('#previous').show()
-				$('#next').show()
+				$('#previous').show();
+				$('#next').show();
 			}
-		})   
+		});   
 	}
-})
+});
