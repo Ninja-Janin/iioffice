f1cd446484a4da2b1f9b2023d7e90d45d54e393e
Luke Magsoling <lukemagsoling@gmail.com>
2018-03-14 15:01:52 +0800

    slider fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
-	var triggers = $('ul.triggers li');
-	var images = $('.slider__photo-image');
-	var lastElem = triggers.length-1;
-	var slider = $('.slider__photo');
-	var target;
-	var lastTarget;
+	var triggers = $('ul.triggers li'),
+		images   = $('.slider__photo-image'),
+		lastElem = triggers.length-1,
+		slider   = $('.slider__photo'),
+		target,
+		lastTarget
 
 	slider.width((100 * images.length) + '%');
 	images.width((100 / images.length) + '%');
@@ -12,12 +12,16 @@ $(document).ready(() => {
 
 	function sliderResponse(target) {
 		lastTarget = target;
-
 		slider.animate({
 			left: (lastTarget * -100) + '%'
 		})
-
 	    triggers.removeClass('active').eq(target).addClass('active');
+	}
+
+	function sliderTiming() {
+	    target = $('ul.triggers li.active').index();
+	    target === lastElem ? target = 0 : target = target+1;
+	    sliderResponse(target);
 	}
 
 	triggers.click(function() {
@@ -45,12 +49,6 @@ $(document).ready(() => {
 			sliderResponse(target);
 		}
 	});
-
-	function sliderTiming() {
-	    target = $('ul.triggers li.active').index();
-	    target === lastElem ? target = 0 : target = target+1;
-	    sliderResponse(target);
-	}
 
 	$('#scroll-top-btn').on('click', () => {
 		$('html, body').animate({ scrollTop: 0 }, 'slow');
